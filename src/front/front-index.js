7f94f19a12bb16d4671e6a2b878f5b2dbb80f1d2
Tristan Conant <tconant@integralgis.local>
2020-02-21 11:20:04 -0800

    Adding polyfill for ie11. rolledd back string search package version aldo for ie11.

--- src/front/front-index.js
+++ src/front/front-index.js
@@ -3,6 +3,8 @@
  *
  * src/front/front-index.js
  */
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
